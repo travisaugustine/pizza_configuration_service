032e6366866009feaf2c1009234e1ba326df4c05
Travis Augustine <traviswilliamaugustine@gmail.com>
2023-05-19 13:33:29 -0500

    add has_many association

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,4 @@
 class Pizza < ApplicationRecord
+  has_many :pizza_toppings
+  has_many :pizzas, through: :pizza_toppings
 end
--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,2 +1,6 @@
 class Topping < ApplicationRecord
+  has_many :pizza_toppings
+  has_many :pizzas, through: :pizza_toppings
+
+  validates :name, uniqueness: { case_sensitive: false }
 end
