7e61b3e48cbc680886ff801ec43fb44c76d4c537
Travis Augustine <traviswilliamaugustine@gmail.com>
2023-05-24 10:34:19 -0500

    Add name uniqueness to pizza

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,4 +1,6 @@
 class Pizza < ApplicationRecord
   has_many :pizza_toppings, dependent: :destroy
   has_many :toppings, through: :pizza_toppings
+
+  validates :name, uniqueness: { case_sensitive: false }
 end
